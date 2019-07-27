1f5e4262f8636149687ae2d8f2942b7e9ad0997e
J-A-C-G <ja-carrerag@hotmail.com>
2019-07-27 14:30:14 -0500

    Mensajes de buenas Tardes

--- src/javacupgithub/JavaCUPGitHub.java
+++ src/javacupgithub/JavaCUPGitHub.java
@@ -16,7 +16,7 @@ public class JavaCUPGitHub {
      */
     public static void main(String[] args) {
         
-        System.out.println("Hola");        
+        System.out.println("Hello Git");    
     }
     
     
